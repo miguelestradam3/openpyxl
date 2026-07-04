4b1fe06e0f6687d3723d0c2f498272a76fbfa20f
unknown <miguelestradam36@gmail.com>
2026-07-03 22:58:33 -0600

    Minor change

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     data_manager = DataManager()
 
     data_manager.database = str(BASE_DIR / "data" / "db" / "CarSalesData.db")
-    data_manager.filename_ = str(BASE_DIR / "data" / "Training.xlsx")
+    data_manager.filename = str(BASE_DIR / "data" / "Training.xlsx")
     data_manager.all_db_to_excel()
 
 
